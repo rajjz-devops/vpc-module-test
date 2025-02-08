12faad1c8b1d3cd0a4605f715bd82e7320cd651c
raju.gaddam <raju.qa32@gmail.com>
2025-02-07 22:47:06 -0500

    script for testing vpc module created

--- vpc.tf
+++ vpc.tf
@@ -5,4 +5,7 @@ module "vpc" {
     vpc_cidr = var.vpc_cidr
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 }
